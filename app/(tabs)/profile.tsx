44bc6dd4466f38b49de811c7a76b2b4f859f189a
Francesco Peluso <59664699+francescopeluso@users.noreply.github.com>
2025-05-13 00:31:43 +0200

    Refactoring

--- app/(tabs)/profile.tsx
+++
@@ -1,7 +0,0 @@
-// This file redirects to the Italian version (profilo.tsx)
-import { Redirect } from 'expo-router';
-import React from 'react';
-
-export default function ProfileScreen() {
-  return <Redirect href="/profilo" />;
-}
